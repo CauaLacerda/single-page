c2d97825320870c916f1738d432a604c45f93132
Cauã Alves Lacerda <115109611+CauaLacerda@users.noreply.github.com>
2023-03-28 17:17:42 -0300

    Update code.js

--- js/code.js
+++ js/code.js
@@ -2,8 +2,8 @@
 lightbox.option({
     'imageFadeDuration': 300,
     'fadeDuration': 300,
-    'albumLabel':  "Foto %1 do total de %2", 
-    'resizeDuration' : 400
+    'albumLabel': "Foto %1 do total de %2",
+    'resizeDuration': 400
 })
 
 //Inicialização do plugin AOS animate
@@ -12,4 +12,9 @@ AOS.init();
 //variaveis p/ selecionar os elementos
 const FIELD_IDADE = document.getElementById('idade')
 const TXT_IDADE = document.getElementById('txt-idade')
-console.log(FIELD_IDADE)
+//txt-idade mostra o valor do campo idade
+TXT_IDADE.innerText = FIELD_IDADE.value
+
+FIELD_IDADE.addEventListener('change', () => {
+    TXT_IDADE.innerText = FIELD_IDADE.value
+})
